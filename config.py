8af390b345b6c4b3c659156b15d384b822585df4
labestia2080 <100966021+reymichel2009@users.noreply.github.com>
2022-11-28 20:51:27 -0500

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ SPLIT_FILE = 1024 * 1024 * int(os.environ.get('split_file','99'))
 ROOT_PATH = 'root/'
 ACCES_USERS = os.environ.get('tl_admin_user','reymichel2009').split(';')
 
-static_proxy = 'socks5h://KGGJJJYIJFLIFEYDJFLEKHYGCHDHRHHKCHHGCE'
+static_proxy = 'socks5://KJGEJHYEJGLIFFYJKGKECGYJGICDRJCGLFGELF'
 PROXY = ProxyCloud.parse(static_proxy)
 
 if PROXY:
